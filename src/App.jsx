fe87464ee1f2c5557d85878a5559f9b30e20da54
Fırat Altun <116505991+altunf@users.noreply.github.com>
2024-11-16 00:31:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Sidebar from "./layouts/sidebar-layout";
 import ContentArea from "./layouts/content-area-layout";
 import { WcstProvider } from "./components/context";
+import Maintenance from "../Maintenance";
+
+const operationalMode = (
+  <WcstProvider>
+    <Sidebar />
+    <ContentArea />
+  </WcstProvider>
+);
 
 function App() {
-  return (
-    <WcstProvider>
-      <Sidebar />
-      <ContentArea />
-    </WcstProvider>
-  );
+  return <Maintenance />;
 }
 
 export default App;
