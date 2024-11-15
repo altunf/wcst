3739fdabbef579cdd49685205c3e5240cd4fd66f
Fırat Altun <116505991+altunf@users.noreply.github.com>
2024-11-16 00:28:10 +0300

    Create Maintenance.jsx

---
+++ Maintenance.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Maintenance = () => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      textAlign: "center",
+    }}
+  >
+    <h1>
+      Site şu anda bakımda.
+      <br />
+      Yakında tekrar öğrencilerin ve profesyonellerin kullanımına açılacaktır.
+    </h1>
+  </div>
+);
+
+export default Maintenance;
